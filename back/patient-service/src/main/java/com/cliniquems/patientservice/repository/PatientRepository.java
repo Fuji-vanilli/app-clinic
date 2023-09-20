848418a09561b56cb0fdf9a4b430eb65bd99d6ad
Fuji vanilli <ijufillivan@gmail.com>
2023-09-20 11:27:53 +0000

    patient repository

---
+++ back/patient-service/src/main/java/com/cliniquems/patientservice/repository/PatientRepository.java
@@ -0,0 +1,14 @@
+package com.cliniquems.patientservice.repository;
+
+import com.cliniquems.patientservice.model.Patient;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface PatientRepository extends MongoRepository<Patient, String> {
+    Optional<Patient> findByCode(String code);
+    void deleteByCode(String code);
+    boolean existsByCode(String code);
+}

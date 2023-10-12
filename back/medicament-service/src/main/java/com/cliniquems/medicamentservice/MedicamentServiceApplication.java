272de9aef4a7e2b5147d592baebdaafa50133a5c
Fuji vanilli <ijufillivan@gmail.com>
2023-10-12 13:24:42 +0000

    medicament service

---
+++ back/medicament-service/src/main/java/com/cliniquems/medicamentservice/MedicamentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cliniquems.medicamentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicamentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicamentServiceApplication.class, args);
+	}
+
+}
